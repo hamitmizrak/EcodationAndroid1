650f8084f52890c5bf3d596798c46b447a0d5d5e
hamitmizrak <hamitmizrak@gmail.com>
2022-07-17 20:37:35 +0300

    first examples

--- app/src/main/java/hamitmizrak/com/MainActivity.java
+++ app/src/main/java/hamitmizrak/com/MainActivity.java
@@ -4,41 +4,64 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Global Variable
+    TextView textView;
+    EditText editText;
+    Button button;
+
 
     //projemiz açıldığında ilk görünen sayfadır.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("onCreate","ilk açılan");
+        //Log.e("onCreate", "ilk açılan");
+
+        //id almamız
+        textView=findViewById(R.id.textViewExam1);
+        editText=findViewById(R.id.editTextExamp1);
+        button=findViewById(R.id.buttonExamp1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("Title","Click success");
+                String editData=editText.getText().toString();
+                textView.setText(editData);
+            }
+        });
     }
 
-    //uygulamaladan çıkıp geri geldiğimde
+/*    //uygulamaladan çıkıp geri geldiğimde
     @Override
     protected void onStart() {
         super.onStart();
-        Log.e("onStart","başladı");
+        Log.e("onStart", "başladı");
     }
 
     //uygulamaladan çıktığımız an çalışan metot
     @Override
     protected void onStop() {
         super.onStop();
-        Log.e("onStop","bitti");
+        Log.e("onStop", "bitti");
     }
 
     //uygulamaladan çıktığımız an çalışan metot
     @Override
     protected void onPause() {
         super.onPause();
-        Log.e("onPause","pause");
+        Log.e("onPause", "pause");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.e("onDestroy","öldü");
-    }
+        Log.e("onDestroy", "öldü");
+    }*/
 }
---
+++ app/src/main/java/hamitmizrak/com/util/Link.java
@@ -0,0 +1,7 @@
+package hamitmizrak.com.util;
+
+public class Link {
+
+    //Circle Image View
+    //https://github.com/hdodenhof/CircleImageView
+}
